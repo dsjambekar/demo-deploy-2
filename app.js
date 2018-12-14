13d5a64b91fce3567c26e666c57c7d1c52aef65c
Dhanashree Jambekar - Freetime Company <d.Jambekar@FreeTimeCompany.local>
2018-12-14 14:30:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+
+// Set up mongoose connection
+const mongoose = require('mongoose');
+let dev_db_url = 'mongodb://root:root123@ds133162.mlab.com:33162/productstutorial';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.set('useCreateIndex', true)
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+var apiRouter = require('./routes/book');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/books', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/book-details/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/book-create', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/book-edit/:id', express.static(path.join(__dirname, 'dist/mean-angular6')));
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.status);
+});
+
+module.exports = app;
